ced44d5a4687d0860a668c3541bdb8b9f8b12a4c
garymck <garymckinnon07@gmail.com>
2017-01-31 21:24:17 +0530

    Update settings.py

--- source/source/settings.py
+++ source/source/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'techarenaoffcl@gmail.com'
-EMAIL_HOST_PASSWORD = 'sasnnjpa?ahsu123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = False
 
